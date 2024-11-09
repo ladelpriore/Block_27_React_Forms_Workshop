d3472d301480baeef23f6759629e82eceeef00f0
Lucas Del Priore <lucasdelpriore@gmail.com>
2024-11-08 18:58:19 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-//Navigate to App.jsx and remove boilerplace code and the unused import statements, leaving a blank application
-// Import both of these components into your App.jsx file, and mount them inside your JSX. 
+/*Step 1, Initialize Project:
+  Navigate to App.jsx and remove boilerplace code and the unused import statements, leaving a blank application
+  Import both of these components into your App.jsx file, and mount them inside your JSX. 
 
-/*Step 3, Token and Authnetication:
-Import useState from react using a hook, and create token and setToken variables with useState.
-Pass these values to each component by passing the setToken function to the SignUpForm component and by passing the token value to the Authenticate component. 
-
-
+Step 3, Token and Authentication:
+  Import useState from react using a hook, and create token and setToken variables with useState.
+  Pass these values to each component by passing the setToken function to the SignUpForm component and by passing the token value to the Authenticate component. 
 */
+
 import { useState } from "react";
 import Authenticate from "./components/Authenticate";
 import SignUpForm from "./components/SignUpForm";
